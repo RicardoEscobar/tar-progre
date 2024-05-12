f762802c864bd20c05a866613fc8794e149e44b4
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-05-12 13:06:01 -0600

    Add Changes to be committed: 	new file:   tarprogre/__init__.py 	new file:   tarprogre/controller/packing.py 	new file:   tests/__init__.py 	new file:   tests/unit/__init__.py 	new file:   tests/unit/test_packing.py

---
+++ tests/unit/test_packing.py
@@ -0,0 +1,19 @@
+"""This module contains tests for the packing module."""
+import unittest
+import tarprogre.controller.packing
+
+
+class TestPacking(unittest.TestCase):
+    """Tests for the packing module."""
+
+    def test_pack(self):
+        """Test the pack function."""
+        pass
+
+    def test_unpack(self):
+        """Test the unpack function."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
